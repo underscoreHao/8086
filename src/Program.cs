f2e488ffd8673dedc7d04210f6d498ec60bcd51d
Pavel Danov <pavel.danov@outlook.com>
2023-03-10 23:11:49 +0000

    8 and 16 bit immediate to register

--- src/Program.cs
+++ src/Program.cs
@@ -1,66 +1,128 @@
 ﻿using System.Text;
 
-Console.WriteLine("8086 v0.1");
-Console.WriteLine();
+class Program
+{
+	private static readonly Dictionary<int, string> instructionEncodings = new()
+	{
+		{ 0b100010, "mov" },
+		{ 0b1011, "mov" }
+	};
+	private static int[] modFieldEncodings = new int[] { 0b00, 0b01, 0b10, 0b11 };
 
-using var stdin = Console.OpenStandardInput();
-using var stdout = Console.OpenStandardOutput();
+	private static readonly Dictionary<int, string[]> regFieldEncodings = new()
+	{
+		{ 0b000, new[] {"al", "ax"} },
+		{ 0b001, new[] {"cl", "cx"} },
+		{ 0b010, new[] {"dl", "dx"} },
+		{ 0b011, new[] {"bl", "bx"} },
+		{ 0b100, new[] {"ah", "sp"} },
+		{ 0b101, new[] {"ch", "bp"} },
+		{ 0b110, new[] {"dh", "si"} },
+		{ 0b111, new[] {"bh", "di"} },
+	};
 
-byte[] buffer = new byte[256];
-int bufferSize = stdin.Read(buffer, 0, buffer.Length);
+	static void Main(string[] args)
+	{
+		Console.WriteLine("8086 v0.1");
+		Console.WriteLine();
+
+		using var stdin = Console.OpenStandardInput();
+		using var stdout = Console.OpenStandardOutput();
+
+		byte[] buffer = new byte[256];
+		int bufferSize = stdin.Read(buffer, 0, buffer.Length);
 
 #if DEBUG
 
-for (int i = 0; i < bufferSize; i += 2)
-	Console.WriteLine(Convert.ToString(buffer[i], 2) + ", " + Convert.ToString(buffer[i + 1], 2));
+		for (int i = 0; i < bufferSize - 1; i += 2)
+			Console.WriteLine(Convert.ToString(buffer[i], 2) + ", " + Convert.ToString(buffer[i + 1], 2));
 
-Console.WriteLine();
+		Console.WriteLine();
 
 #endif
 
-#region OPCODE DECODER
+		var stringBuilder = new StringBuilder();
+		stringBuilder.AppendLine("bits 16\n");
 
-var instructionEncodings = new Dictionary<int, string>()
-{
-	{ 0b100010, "mov" }
-};
+		for (int i = 0; i < bufferSize; i += 2)
+		{
+			Console.WriteLine(Convert.ToString(buffer[i], 2) + ", " + Convert.ToString(buffer[i + 1], 2));
 
-var regFieldEncodings = new Dictionary<int, string[]>()
-{
-	{ 0b000, new[] {"al", "ax"} },
-	{ 0b001, new[] {"cl", "cx"} },
-	{ 0b010, new[] {"dl", "dx"} },
-	{ 0b011, new[] {"bl", "bx"} },
-	{ 0b100, new[] {"ah", "sp"} },
-	{ 0b101, new[] {"ch", "bp"} },
-	{ 0b110, new[] {"dh", "si"} },
-	{ 0b111, new[] {"bh", "di"} },
-};
+			var instr = new Instruction();
+			string src = string.Empty;
+			string dest = string.Empty;
 
-var modFieldEncodings = new int[] { 0b00, 0b01, 0b10, 0b11 };
+			instr.OpCode = GetOpCode(buffer[i]);
 
-var stringBuilder = new StringBuilder();
-stringBuilder.AppendLine("bits 16\n");
+			switch (instr.OpCode)
+			{
+				case 0b1011:
+					instr.W_Flag = buffer[i] >> 3 & 1;
+					instr.Reg = buffer[i] >> 0 & 7;
+					if (instr.W_Flag == 0)
+					{
+						instr.Data = (sbyte)buffer[i + 1];
+					}
+					else
+					{
+						instr.Data = (short)((buffer[i + 2] << 8) | (buffer[i + 1] << 0));
+						i += 1; // skip next two bytes as they've been read
+					}
+					dest = regFieldEncodings[instr.Reg][instr.W_Flag];
+					src = instr.Data.ToString();
 
-for (int i = 0; i < bufferSize; i += 2)
-{
-	int opCode = buffer[i] >> 2;
-	int d = buffer[i] >> 1 & 3;
-	int w = buffer[i] >> 0 & 1;
+					break;
+				case 0b100010:
+					instr.D_Flag = buffer[i] >> 1 & 3;
+					instr.W_Flag = buffer[i] >> 0 & 1;
+					instr.Mod = buffer[i + 1] >> 6 & 7;
+					instr.Reg = buffer[i + 1] >> 3 & 7;
+					instr.Rm = buffer[i + 1] >> 0 & 7;
 
-	int mod = buffer[i + 1] >> 6 & 7;
-	int reg = buffer[i + 1] >> 3 & 7;
-	int rm = buffer[i + 1] >> 0 & 7;
+					src = instr.D_Flag == 0
+						? regFieldEncodings[instr.Reg][instr.W_Flag]
+						: regFieldEncodings[instr.Rm][instr.W_Flag];
 
-	// TODO: This will fail horribly if the opCode is not `mov`
-	stringBuilder.Append(instructionEncodings[opCode] + " ");
+					dest = instr.D_Flag == 0
+						? regFieldEncodings[instr.Rm][instr.W_Flag]
+						: regFieldEncodings[instr.Reg][instr.W_Flag];
 
-	string src = d == 0 ? regFieldEncodings[reg][w] : regFieldEncodings[rm][w];
-	string dest = d == 0 ? regFieldEncodings[rm][w] : regFieldEncodings[reg][w];
+					break;
+			}
 
-	stringBuilder.Append($"{dest}, {src}\n");
+			Console.WriteLine($"{dest}, {src}");
+
+			stringBuilder.Append(instructionEncodings[instr.OpCode] + " ");
+			stringBuilder.Append($"{dest}, {src}\n");
+		}
+
+		Console.WriteLine(stringBuilder.ToString());
+	}
+
+	static int GetOpCode(int byte1)
+	{
+		// check if we're doing immediate-to-register first
+		var opCode = byte1 >> 4;
+		if (instructionEncodings.ContainsKey(opCode))
+			return opCode;
+
+		// check if we're doing register/memory to/from register
+		opCode = byte1 >> 2;
+		if (instructionEncodings.ContainsKey(opCode))
+			return opCode;
+
+		// TODO: Will blow up if opCode is not found
+		return 0;
+	}
 }
 
-Console.WriteLine(stringBuilder.ToString());
-
-#endregion
+public struct Instruction
+{
+	public int OpCode { get; set; }
+	public int D_Flag { get; set; }
+	public int W_Flag { get; set; }
+	public int Mod { get; set; }
+	public int Reg { get; set; }
+	public int Rm { get; set; }
+	public int Data { get; set; }
+}
